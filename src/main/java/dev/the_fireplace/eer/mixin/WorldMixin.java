dc67c6dff4519501bfba564f06017367856f3865
The_Fireplace <git@the-fireplace.dev>
2022-06-09 18:09:46 -0500

    Update gradle, update buildscript, switch to Mojmap

--- src/main/java/dev/the_fireplace/eer/mixin/WorldMixin.java
+++ src/main/java/dev/the_fireplace/eer/mixin/WorldMixin.java
@@ -1,12 +1,12 @@
 package dev.the_fireplace.eer.mixin;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.util.crash.CrashException;
-import net.minecraft.util.crash.CrashReport;
-import net.minecraft.util.crash.CrashReportSection;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
+import net.minecraft.CrashReport;
+import net.minecraft.CrashReportCategory;
+import net.minecraft.ReportedException;
+import net.minecraft.core.Registry;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.Overwrite;
 
 import java.util.function.Consumer;
 
-@Mixin(World.class)
+@Mixin(Level.class)
 public abstract class WorldMixin {
     private static final Logger LOGGER = LogManager.getLogger("eer");
 
@@ -28,15 +28,15 @@ public abstract class WorldMixin {
             tickConsumer.accept(entity);
         } catch (Throwable throwable) {
             try {
-                LOGGER.warn("Removing erroring entity of type {} at {} :", Registry.ENTITY_TYPE.getId(entity.getType()).toString(), entity.getPos().toString());
-                LOGGER.warn(entity.writeNbt(new NbtCompound()).toString());
+                LOGGER.warn("Removing erroring entity of type {} at {} :", Registry.ENTITY_TYPE.getKey(entity.getType()).toString(), entity.position().toString());
+                LOGGER.warn(entity.saveWithoutId(new CompoundTag()).toString());
                 entity.remove(Entity.RemovalReason.DISCARDED);
                 LOGGER.error("Erroring Entity Stacktrace:", throwable);
             } catch(Exception e) {
-                CrashReport crashReport = CrashReport.create(throwable, "Ticking entity");
-                CrashReportSection crashReportSection = crashReport.addElement("Entity being ticked");
-                entity.populateCrashReport(crashReportSection);
-                throw new CrashException(crashReport);
+                CrashReport crashReport = CrashReport.forThrowable(throwable, "Ticking entity");
+                CrashReportCategory crashReportSection = crashReport.addCategory("Entity being ticked");
+                entity.fillCrashReportCategory(crashReportSection);
+                throw new ReportedException(crashReport);
             }
         }
     }
