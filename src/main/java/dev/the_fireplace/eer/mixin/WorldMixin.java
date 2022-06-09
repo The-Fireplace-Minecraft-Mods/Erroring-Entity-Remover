a3baa6d395602ead9ebca1486cc90b7d4727a006
The_Fireplace <git@the-fireplace.dev>
2022-06-08 20:06:16 -0500

    Add entity type to the error log when removing an entity

--- src/main/java/dev/the_fireplace/eer/mixin/WorldMixin.java
+++ src/main/java/dev/the_fireplace/eer/mixin/WorldMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.crash.CrashException;
 import net.minecraft.util.crash.CrashReport;
 import net.minecraft.util.crash.CrashReportSection;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,7 +28,7 @@ public abstract class WorldMixin {
             tickConsumer.accept(entity);
         } catch (Throwable throwable) {
             try {
-                LOGGER.warn("Removing erroring entity at {} :", entity.getPos().toString());
+                LOGGER.warn("Removing erroring entity of type {} at {} :", Registry.ENTITY_TYPE.getId(entity.getType()).toString(), entity.getPos().toString());
                 LOGGER.warn(entity.writeNbt(new NbtCompound()).toString());
                 entity.remove(Entity.RemovalReason.DISCARDED);
                 LOGGER.error("Erroring Entity Stacktrace:", throwable);
